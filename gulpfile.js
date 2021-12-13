88d5dcc834ce0819be4178fb2f84862fd6f1de3f
Igor Bodrov <bodrovigor02@gmail.com>
2021-12-13 18:56:06 +0300

    Исправление ошибок

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,9 @@ const concat 														   = require('gulp-concat');
 const browserSync 												 = require('browser-sync').create();
 const terser 															 = require('gulp-terser');
 const babel 															 = require('gulp-babel');
-const imagemin 														 = require('gulp-imagemin');
-const recompress 													 = require('imagemin-jpeg-recompress');
-const pngquant 														 = require('imagemin-pngquant');
-const webpConv 														 = require('gulp-webp');
 const plumber 														 = require('gulp-plumber');
+const del 																 = require('del');
+const gulpSquoosh 												 = require('gulp-squoosh');
 
 // Обновление Html в папке билд
 function copyHtml() {
@@ -58,41 +56,20 @@ function copyImg() {
 		.pipe(browserSync.stream())
 }
 
-// Минификация изображений - не работает ¯\_(ツ)_/¯
-function minImg() {
-	return src('src/img/content/*.+(png|jpg|jpeg|gif|svg|ico)')
-		.pipe(plumber())
-		.pipe(imagemin({
-				interlaced: true,
-				progressive: true,
-				optimizationLevel: 5,
-			},
-			[
-				recompress({
-					loops: 6,
-					min: 50,
-					max: 90,
-					quality: 'high',
-					use: [pngquant({
-						quality: [0.8, 1],
-						strip: true,
-						speed: 1
-					})],
-				}),
-				imagemin.gifsicle(),
-				imagemin.optipng(),
-				imagemin.svgo()
-			], ), )
+// Минификация изображения, конвертация в webp
+function processImages() {
+	return src('src/img/content/**/*')
+		.pipe(gulpSquoosh({
+					encodeOptions: {
+						webp: {},
+						...('src/img/content/**/*' === ".png"
+							? { oxipng: {} }
+							: { mozjpeg: {} }),
+					}
+				})
+		)
 		.pipe(dest('build/img/content'))
 		.pipe(browserSync.stream())
-}
-
-// Конвертация изображений в Webp
-function imgToWebp() {
-	return src('build/img/content/*.+(png|jpg|jpeg)')
-		.pipe(plumber())
-		.pipe(webpConv())
-		.pipe(dest('build/img/content'))
 }
 
 // Слежение за проектом
@@ -112,5 +89,9 @@ function syncBrowser() {
 	});
 }
 
+function deleteBuild() {
+	return del('build')
+}
+
 exports.default = parallel(copyHtml, minStyle, minJs, copyImg, watching, syncBrowser);
-exports.build 	= series(copyHtml, minStyle, minJs, minImg, imgToWebp);
+exports.build 	= series(deleteBuild, copyHtml, minStyle, minJs, processImages);
