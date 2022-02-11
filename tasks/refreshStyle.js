6bfb7577fd92d494848f089cc966aef35fe5cb69
Igor Bodrov <bodrovigor02@gmail.com>
2022-02-11 15:19:05 +0300

    fix: 🐛 Ошибка с задачей
    Исправил ошибку в задаче обновления стилей

--- tasks/refreshStyle.js
+++ tasks/refreshStyle.js
@@ -1,9 +1,11 @@
 const {src, dest} = require('gulp');
 const concat 		  = require('gulp-concat');
+const scss  			= require('gulp-sass')(require('sass'));
 
 // Обновление CSS в папке build
 module.exports = function refreshStyle(browserSync) {
 	return src('src/scss/**/*.scss')
+		.pipe(scss())
 		.pipe(concat('style.min.css'))
 		.pipe(dest('build/css/'))
 		.pipe(browserSync.stream())
